6b965aec2b1b8f11211d70d70e84f9907626b91f
jdleong <114703353+jdleong@users.noreply.github.com>
2023-03-02 15:04:30 -0500

    Initial commit

---
+++ frontend/build/precache-manifest.6a7b6b27845fae6d3aef651165bbd676.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef45732c3708203d5f3b4ca70afacbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97320bfb77a7a2c0b44f",
+    "url": "/static/css/2.38dcfae8.chunk.css"
+  },
+  {
+    "revision": "97320bfb77a7a2c0b44f",
+    "url": "/static/js/2.49d80d44.chunk.js"
+  },
+  {
+    "revision": "093bf3f86145069d5ad0de50dd236547",
+    "url": "/static/js/2.49d80d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf3d173158526f70b8f3",
+    "url": "/static/js/3.b93f18ee.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.b93f18ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23056f350c98d492ac55",
+    "url": "/static/js/main.abcf8a8f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.abcf8a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26727e7b167f7287ec97",
+    "url": "/static/js/runtime-main.dbe99236.js"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/static/media/wa-background.adbccee0.png"
+  }
+]);
